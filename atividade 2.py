c085d72ff83c50f665b365fb33195cf15bd285a3
KelviAraujo <kelveinjecao.ti@gmail.com>
2026-01-29 21:15:40 -0300

    aulas iniciais

---
+++ atividade 2.py
@@ -0,0 +1,25 @@
+nome = " Meu nome é kelvi"
+idade =  "tenho 19 anos é"
+altura = "tenho 1,76 de altura"
+
+nome1 = " Meu nome é maria"
+idade1 =  "tenho 28 anos é"
+altura1 = "tenho 1,80 de altura"
+
+nome2 = " Meu nome é joao"
+idade2 =  "tenho 14 anos é"
+altura2 = "tenho 1,60 de altura"
+
+nome3 = " Meu nome é kelvi"
+idade3 =  "tenho 19 anos é"
+altura3 = "tenho 1,76 de altura"
+
+nome4 = " Meu nome é ricardo"
+idade4 =  "tenho 80 anos é"
+altura4 = "tenho 1,67de altura"
+
+nome5 = " Meu nome é julia"
+idade5 =  "tenho 34 anos é"
+altura5 = "tenho 1,72 de altura"
+
+print (nome, idade, altura, nome1, idade1, altura1)
---
+++ ola mundo.py
@@ -0,0 +1,6 @@
+texto = "ola mundo" 
+num5 = 5 
+texto2 = "bem vindo ao python"
+hoje = "hoje é 28/01/2006"
+
+print(texto, texto2, hoje)
